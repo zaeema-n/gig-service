a21744d32852e4e5b640a3ebd3ed2d68fccf9f83
zaeema-n <96104978+zaeema-n@users.noreply.github.com>
2024-11-20 10:46:15 +0530

    Update init.go

--- app/controllers/api/init.go
+++ app/controllers/api/init.go
@@ -7,9 +7,9 @@ import (
 )
 
 func init() {
-	// revel.InterceptFunc(authentication.Authenticate, revel.BEFORE, &UserController{})
-	// revel.InterceptFunc(authentication.Authenticate, revel.BEFORE, &PublisherController{})
-	// revel.InterceptFunc(authentication.Authenticate, revel.BEFORE, &EntityEditController{})
-	// revel.InterceptFunc(authentication.Authenticate, revel.BEFORE, &TokenValidationController{})
-	// revel.InterceptFunc(authentication.Authenticate, revel.BEFORE, &FileUploadController{})
+	revel.InterceptFunc(authentication.Authenticate, revel.BEFORE, &UserController{})
+	revel.InterceptFunc(authentication.Authenticate, revel.BEFORE, &PublisherController{})
+	revel.InterceptFunc(authentication.Authenticate, revel.BEFORE, &EntityEditController{})
+	revel.InterceptFunc(authentication.Authenticate, revel.BEFORE, &TokenValidationController{})
+	revel.InterceptFunc(authentication.Authenticate, revel.BEFORE, &FileUploadController{})
 }
